c18a0001c8a5988b3484c246c5e38fc88d19150a
Here-Be-Dragons <jeradj@gmail.com>
2016-05-19 20:41:29 -0400

    First attempt at compilable code

--- Pool-Controller.ino
+++ Pool-Controller.ino
@@ -4,6 +4,9 @@
 // Refer to README.md for additional information
 // https://github.com/Here-Be-Dragons/Pool-Controller
 
+// TODO: Implement Energy Consumption Info
+// TODO: Fix SmartThings override
+
 ////
 //Start of user configurations
 ////
@@ -20,7 +23,7 @@ uint16_t speedRPM[8] = {0,600,1200,1800,2300,2750,3000,3450};
 // Manually trigger each of the above speeds and record their
 // energy usage below for accurate consumption tracking
 uint16_t energyConsum[8] = {5,100,200,300,400,500,600,700};
-uint16_t expiryLength = 60; //How long to run manual Overrides
+uint16_t expiryLength = 60; //How long in minutes to run manual Overrides
 
 // Speed activation times. If two speeds have the same
 // time entered, the higher speed takes precidence.
@@ -37,13 +40,14 @@ uint16_t aSpeed8[] = {                        }
 //End of user configurations
 ////
 
-uint16_t currentTime;     //The current time
-uint16_t currentSpeed = 1;    //The current motor speed
-uint16_t lastChange;      //Time the speed was last changed via scheduler
-uint8_t newSpeed = 1;       //Valid values 1-8, relates to the speed setting to call
+uint16_t currentTime;         //The current time
+uint16_t currentSpeed;        //The current motor speed
+uint16_t overrideSpeed;       //Stores the override speed when set manually
+uint16_t lastChange;          //Time the speed was last changed via scheduler
+uint8_t scheduledSpeed = 0;   //What speed the schedule says you should be at
 bool autoOverride = 0;        //0 for scheduled, 1 for override.  Changes when solar kicks on
 bool manualOverride = 0;      //0 for scheduled, 1 for override.  Changes from user intervention
-uint16_t OverrideStarted = 0;  //This is set when a manual override is triggered 
+uint16_t OverrideStarted = 0; //This is set to currentTime when a manual override is triggered 
 
 // Assign pins to relays
 // D0 reserved for SDA of OLED
@@ -62,83 +66,92 @@ void setup() {
     pinMode(pPumpRelay3,  OUTPUT);
     pinMode(pSolarRelay1, INPUT );
 
-    oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64 screen)
-    oled.display(); // show splashscreen
-    Particle.function("mOverride", mOverride); //Force speed from user input
-    Time.zone(-5);
-    delay(5000); //Give it time to stabilize the RTC and get a time from NTP
-    setPumpSpeed(1);
+    oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);    // Initialize with the I2C addr 0x3D (for the 128x64 screen)
+    oled.display();                            // Show splashscreen
+    Particle.function("mOverride", mOverride); // Listen for a manual override via remote user input
+    Time.zone(-5);			       // Ignores DST... :(
+    delay(5000);                               // Give it time to stabilize the RTC and get a time from NTP
+    returnToSchedule();                        // Find what the current speed should be
 }
 
 void loop() {
   currentTime = Time.hour() * 100 + Time.minute();
   //checkAutoOverride(); //Need a way to check if we should be runing in auto override
-  if( manualOverride == 1 && OverrideStarted <= currentTime + expiryLength ){
-    endOverride();
+
+  //Check for expired manual Override
+  if( manualOverride == 1 && OverrideStarted + expiryLength <= currentTime ) {
+    returnToSchedule();
   }
-  newSpeed = findScheduledSpeed(currentTime);
-  if( newspeed != 0 && newSpeed != currentSpeed ){
-    setPumpSpeed(newSpeed);
-  }
-  oled.clearDisplay();
-  oled.setTextSize(1);
-  oled.setTextColor(WHITE);
-  oled.setCursor(0,0);
-  oled.println("5-hr rain forecast:");
-  oled.println("Units: Inches");
-  oled.println();
-  oled.setTextSize(3);
-  oled.print(precipInSum);
-  oled.display();
+
+  //Check for new scheduled speed
+  scheduledSpeed = findScheduledSpeed(currentTime);
+
+  //Check for Solar Speed Override.  Applies in setPumpSpeed()
+  autoOverride = digitalRead(pSolarRelay1);
+
+  //Activate new speed if there is one
+  setPumpSpeed();
+
+  //Update the oled display
+  updateDisplay();
 }
 
 int findScheduledSpeed(uint16_t atTime){ // Find the Scheduled Speed
+  int x;
   for (int i=0; i < sizeof(aSpeed1) / sizeof(uint16_t); i++) {
     if ( aSpeed1[i] == atTime ) {
-      newSpeed = 1;
+      x = 1;
     }
   }
   for (int i=0; i < sizeof(aSpeed2) / sizeof(uint16_t); i++) {
     if ( aSpeed2[i] == atTime ) {
-      newSpeed = 2;
+      x = 2;
     }
   }
   for (int i=0; i < sizeof(aSpeed3) / sizeof(uint16_t); i++) {
     if ( aSpeed3[i] == atTime ) {
-      newSpeed = 3;
+      x = 3;
     }
   }
   for (int i=0; i < sizeof(aSpeed4) / sizeof(uint16_t); i++) {
     if ( aSpeed4[i] == atTime ) {
-      newSpeed = 4;
+      x = 4;
     }
   }
   for (int i=0; i < sizeof(aSpeed5) / sizeof(uint16_t); i++) {
     if ( aSpeed5[i] == atTime ) {
-      newSpeed = 5;
+      x = 5;
     }
   }
   for (int i=0; i < sizeof(aSpeed6) / sizeof(uint16_t); i++) {
     if ( aSpeed6[i] == atTime ) {
-      newSpeed = 6;
+      x = 6;
     }
   }
   for (int i=0; i < sizeof(aSpeed7) / sizeof(uint16_t); i++) {
     if ( aSpeed7[i] == atTime ) {
-      newSpeed = 7;
+      x = 7;
     }
   }
   for (int i=0; i < sizeof(aSpeed8) / sizeof(uint16_t); i++) {
     if ( aSpeed1[8] == atTime ) {
-      newSpeed = 8;
+      x = 8;
     }
   }
-  return newSpeed;
+  return x;
 }
 
-void setPumpSpeed(newSpeed) {
-  if (manualOverride != 1) { // Is there an active manual override?
-    if (autoOverride != 1) { // Is there an active automatic override?
+void setPumpSpeed() {
+  uint8_t newSpeed;
+  newspeed = scheduledSpeed;
+  //Set Manual Override Speed to newSpeed if active
+  if( manualOverride == 1 ){
+    newSpeed = overrideSpeed;
+  }
+  //If auto override is active, set a minimum speed
+  if( autoOverride == 1 && newSpeed < 6 ){
+    newSpeed = 6;
+  }
       switch (newSpeed) {
         case 1:
           digitalWrite(pPumpRelay1, LOW );
@@ -180,68 +193,55 @@ void setPumpSpeed(newSpeed) {
           digitalWrite(pPumpRelay2, HIGH);
           digitalWrite(pPumpRelay3, HIGH);
           break;
+	//This condition should never happen, but default to Speed 6 if it does
         default:
+          digitalWrite(pPumpRelay1, HIGH);
+          digitalWrite(pPumpRelay2, LOW );
+          digitalWrite(pPumpRelay3, HIGH);
           break;
       }
-      currentSpeed = newSpeed;
-      newSpeed = 0;
+      currentSpeed = newSpeed; //Update current speed value
 }
 
-int mOverride(String command){ //Triggered by SmartThings
-  uint8_t intCommand = atoi(command);
+int mOverride(String command) { //Triggered by SmartThings
+  overrideSpeed = atoi(command);
   manualOverride = 1;
   overrideStarted = Time.hour() * 100 + Time.minute();
-  //overrideSchedule();
-  setPumpSpeed(intCommand);
+  setPumpSpeed();
 }
 
-void endOverride(){
+void returnToSchedule() {
   int testTime = currentTime;
-  newSpeed = 0;
-  while( newSpeed == 0 ){
-    newSpeed = findScheduledSpeed(testTime);
+  while( scheduledSpeed == 0 ) {
+    scheduledSpeed = findScheduledSpeed(testTime);
     testTime--;
   }
   manualOverride = 0;
 }
-
-int aOverride(){
-  autoOverride = 1
-}
-
-/*void overrideSchedule(){
-  currentTime = Time.hour() * 100 + Time.minute();
-  setPumpSpeed(); //Convert string to int, then use that to set Pump Speed
-
-  if(command == "1"){
-      setPumpSpeed(1);
-  }
-  
-  else if(command == "2"){
-      setPumpSpeed(2);
-  }
-  
-  else if(command == "3"){
-      setPumpSpeed(3);
-  }
-
-  else if(command == "4"){
-      setPumpSpeed(4);
-          
-      }  
-  else if(command == "5") {
-      setPumpSpeed(5);
-          
-      }  
-  else if(command == "6") {
-      setPumpSpeed(6);
-          
-      } 
-  else if(command == "7") {
-      setPumpSpeed(7);
-      }
-  
-  else if(command == "8"){
-      setPumpSpeed(8);
-  }*/
+void updateDisplay(){
+  oled.clearDisplay();
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.setCursor(0,0);
+  oled.println("Current Time: ");
+  oled.print(currentTime);
+  oled.println("Current Speed: ");
+  oled.print(currentSpeed);
+  oled.print(" (");
+  oled.print(speedRPM[currentSpeed-1]);
+  oled.print(" RPM)");
+  oled.println();
+  oled.println("Scheduled Speed: ");
+  oled.print(scheduledSpeed);
+  oled.print(" (");
+  oled.print(speedRPM[ScheduledSpeed-1]);
+  oled.print(" RPM)");
+  oled.println("Manual Override? ");
+  if( manualOverride ) oled.print("YES");
+  else oled.print("no");
+  oled.println("Automatic Override? ");
+  if( autoOverride ) oled.print("YES");
+  else oled.print("no");
+  oled.setTextSize(3);
+  oled.display();
 }
