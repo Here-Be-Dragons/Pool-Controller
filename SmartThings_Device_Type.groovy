fc8bbfa79c4fa7967c7cc7942726194464e4dbd0
Here-Be-Dragons <jeradj@gmail.com>
2016-05-25 20:54:09 -0400

    Still broken but making progress

--- SmartThings_Device_Type.groovy
+++ SmartThings_Device_Type.groovy
@@ -13,161 +13,130 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
-
+ 
 
 
 metadata {
 	definition (name: "Hayward Tristar VS Controller", namespace: "Here-Be-Dragons", author: "Jerad Jacob") {
-		capability "Switch"
-		capability "Polling"
-		capability "Refresh"
-
-		// tile definitions.  must also define a parse method in your device handler with the same name
-		command "off"
-		command "speed2"
-		command "speed3"
-		command "speed4"
-		command "speed5"
-		command "speed6"
-		command "speed7"
-		command "speed8"
-		command "updateData", ["string"]
-		command "resume"
+		capability "Actuator" //Best practice to include if it has commands
+        capability "Sensor" //Best practice to include if it has attributes
+        capability "Polling"
+        capability "Refresh"
+        capability "Relay Switch"
+        
+        command "sendCommand", ["number"]
 	}
 
-	preferences {
-		input("deviceId", "text", title: "Device ID", description: "Particle Device ID", required: true, displayDuringSetup: true)
-		input("token", "text", title: "Access Token", description: "Particle User Access Token", required: true, displayDuringSetup: true)
+	simulator {
+		
 	}
-
-	tiles(scale: 2) {
-		valueTile("icon", "device.icon", width: 2, height: 2) {
-			state("default", label:'${currentValue} RPM', icon:"st.Health & Wellness.health2", backgroundColor: "#7B8BFF")
+    
+    preferences {
+        input("deviceId", "text", title: "Device ID", description: "Particle Device ID", required: false, displayDuringSetup: true)
+        input("token", "text", title: "Access Token", description: "Particle User Access Token", required: false, displayDuringSetup: true)
+    }
+    
+    tiles(scale: 2) {
+		valueTile("icon", "device.currentSpeed", decoration: "flat", width: 2, height: 2) {
+			state("default", label:'${currentValue} RPM', icon:"st.Health & Wellness.health2")
 		}
-		valueTile("currentSpeed", "device.currentSpeed", width: 3, height: 2) {
-			state("default", label:'Currently: ${currentValue} RPM', backgroundColor: "#7B8BFF")
+		valueTile("currentSpeed", "device.currentSpeed", decoration: "flat", width: 3, height: 2) {
+			state("default", label:'Currently: ${currentValue} RPM')
 		}
 		standardTile("off", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
-			state("inactive", action:"setSpeed(1)", label:'Set OFF', nextState:"active", backgroundColor:"#F7C4BA")
-			state("active", action:"setSpeed(1)", label:'Set OFF', backgroundColor:"#F7C4BA")
+			state "inactive", label: 'OFF', action:"sendCommand", nextState: "active"
+            state "active", label: 'OFF', action:"sendCommand", nextState: "inactive", backgroundColor:"#00FF00"
 		}
 		standardTile("speed2", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
-			state("inactive", action:"setSpeed(2)",  label:'Set Speed 2', backgroundColor:"#F7C4BA")
-			state("active", action:"setSpeed(2)", label:'Set Speed 2', backgroundColor:"#F7C4BA")
+			state("inactive", action:"sendCommand",  label:'600')
+			state("active", action:"sendCommand", label:'600')
 		}
 		standardTile("speed3", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
-			state("inactive", action:"setSpeed(3)", label:'Set Speed 3', backgroundColor:"#F7C4BA")
-			state("active", action:"setSpeed(3)", label:'Set Speed 3', backgroundColor:"#F7C4BA")
+			state("inactive", action:"setSpeed(3)", label:'1200')
+			state("active", action:"setSpeed(3)", label:'1200')
 		}
 		standardTile("speed4", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
-			state("inactive", action:"speed4", label:'Set Speed 4', backgroundColor:"#F7C4BA")
-			state("active", action:"speed4", label:'Set Speed 4', backgroundColor:"#F7C4BA")
+			state("inactive", action:"speed4", label:'1800')
+			state("active", action:"speed4", label:'1800')
 		}
 		standardTile("speed5", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
-			state("inactive", action:"speed5", label:'Set Speed 5', backgroundColor:"#F7C4BA")
-			state("active", action:"speed5", label:'Set Speed 5', backgroundColor:"#F7C4BA")
+			state("inactive", action:"speed5", label:'2300')
+			state("active", action:"speed5", label:'2300')
 		}
 		standardTile("speed6", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
-			state("inactive", action:"speed6",   label:'Set Speed 6', backgroundColor:"#F7C4BA")
-			state("active", action:"speed6", label:'Set Speed 6', backgroundColor:"#F7C4BA")
+			state("inactive", action:"speed6",   label:'2750')
+			state("active", action:"speed6", label:'2750')
 		}
 		standardTile("speed7", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
 			state("inactive", action:"speed7", label:'Set Speed 7', backgroundColor:"#F7C4BA")
-			state("active", action:"speed7", label:'Set Speed 7', backgroundColor:"#F7C4BA")
+			state("active", action:"speed7", label:'3000')
 		}
 		standardTile("speed8", "device.setSpeed", decoration: "flat", width: 3, height: 1) {
 			state("inactive", action:"speed8", label:'Set Speed 8', backgroundColor:"#F7C4BA")
-			state("active", action:"speed8", label:'Set Speed 8', backgroundColor:"#F7C4BA")
+			state("active", action:"speed8", label:'3450')
 		}
 		standardTile("refresh", "device.refresh", decoration: "flat", width: 3, height: 1) {
-			state("default", action:"refresh", label:"Refresh Current Speed", icon:"st.secondary.refresh", backgroundColor:"#F7C4BA")
+			state("default", action:"polling.poll", label:'Refresh Current Speed', icon:"st.secondary.refresh")
 		}
 		standardTile("resume", "device.resume", decoration: "flat", width: 3, height: 1) {
-			state("default", action:"resume", label:"Resume Schedule", backgroundColor:"#F7C4BA")
+			state("default", action:"resume", label:'Resume Schedule')
 		}
 
-		main "icon"   
+		main ("icon")   
 		details([
 		"currentSpeed",	"refresh",
-								"resume",
-		"off",					"speed2",
-		"speed3", 			"speed4",
-		"speed5", 			"speed6",
-		"speed7", 			"speed8"
+						"resume",
+		"off",			"speed2",
+		"speed3", 		"speed4",
+		"speed5", 		"speed6",
+		"speed7", 		"speed8"
 		])
 	}
 }
 
-// example
-def parse(String description) {
-	log.debug "parse description: $description"
-}
-/*
+// handle commands
 def poll() {
-	updateDeviceData(parent.getDeviceData(this.device)) }
+	log.debug "Executing 'poll'"
+    getSpeed(10)
+}
 
-def refresh() {
-	log.debug "Manually requested refresh"
-	parent.refresh() }
+def sendCommand(command) {
+	log.debug "Executing ${command}"
+    getspeed(command)
+}
 
-def updateDeviceData(actualData) {
-	def deviceData = convertTemperatureUnit(actualData, getTemperatureScale())
-	sendEvent(name: "heatingSetpoint", value: deviceData.setPoint, unit: getTemperatureScale())
+/*def parse(String description) {
+    log.debug "parse description: $description"
+
+    def attrName = null
+    def attrValue = null
+
+    if (description?.startsWith("on/off:")) {
+        log.debug "switch command"
+        attrName = "switch"
+        attrValue = description?.endsWith("1") ? "on" : "off"
+    }
+
+    def result = createEvent(name: attrName, value: attrValue)
+
+    log.debug "Parse returned ${result?.descriptionText}"
+    return result
 }*/
 
-def setSpeed(x){
-	put ${x}
-}
+// Get the temperature & humidity
+private getSpeed(command) {
+    //Particle API Call
+    def speedClosure = { response ->
+	  	log.debug "Speed Request was successful, $response.data"
+      
+      	sendEvent(name: "currentSpeed", value: response.data.return_value)
+	}
+    
+    def speedParams = [
+  		uri: "https://api.particle.io/v1/devices/${deviceId}/mOverride",
+        body: [access_token: token, command: command],  
+        success: speedClosure
+	]
 
-def off(){
-	log.debug "Set State: OFF"
-	put '1'
-}
-
-def speed2() {
-	log.debug "Set State: Speed 2"
-	put '2'
-}
-
-def speed3() {
-	log.debug "Set State: Speed 3"
-	put '3'
-}
-
-def speed4() {
-	log.debug "Set State: Speed 4"
-	put '4'
-}
-
-def speed5() {
-	log.debug "Set State: Speed 5"
-	put '5'
-}
-
-def speed6() {
-	log.debug "Set State: Speed 6"
-	put '6'
-}
-
-def speed7() {
-	log.debug "Set State: Speed 7"
-	put '7'
-}
-
-def speed8() {
-	log.debug "Set State: Speed 8"
-	put '8'
-}
-
-def resume(){
-	log.debug "Set State: Resume Schedule"
-	put 'resume'
-}
-
-private put(newSpeed) {
-    //Particle Photon API Call
-    httpPost(
-        uri: "https://api.particle.io/v1/devices/${deviceId}/newSpeed",
-        body: [access_token: ${token}, command: newSpeed],  
-    )
+	httpPost(speedParams)
 }
